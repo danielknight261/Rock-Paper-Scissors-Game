2c21877c214a74f128c16af13651017287bbfd2c
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2022-05-20 10:29:55 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,63 @@
+const getUserChoice = userInput => {
+userInput = userInput.toLowerCase();
+if (userInput === 'rock' || userInput === 'scissors' || userInput === 'paper') {
+  return userInput
+} else { 
+  
+ }
+}
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+ switch (randomNumber) {
+ case 0:
+ return 'rock';
+ case 1:
+ return 'paper';
+ case 2:
+ return 'scissors';
+ }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return  'Its a tie';
+  }
+  if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return'Sorry, computer won!';
+    } else {
+      return 'You won!';
+    }
+  }
+
+    if (userChoice === 'paper') {
+      if (computerChoice === 'scissors') {
+         return 'Sorry, computer won!';
+      }  else {
+         return 'You won!';
+     }
+    }
+
+    if (userChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+      return 'Sorry, computer won!';
+  } else {
+    return 'You won!';
+      }
+    } 
+  } 
+  
+const playGame = () => {
+const userChoice = getUserChoice('scissors');
+const computerChoice = getComputerChoice();
+console.log('Your choice is: ' + userChoice);
+console.log('Computer choice is ' + computerChoice);
+
+console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame()
+
+
+
